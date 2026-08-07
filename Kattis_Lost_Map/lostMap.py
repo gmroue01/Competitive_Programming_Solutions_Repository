be7dc6fd6feaaf0422b4856539a9ec8618c0c2e4
gmroue01 <95682994+gmroue01@users.noreply.github.com>
2026-08-07 10:13:13 +0200

    solution (lostmap) : implement the full solution of the problem. It is the prim's algorithm to find a MST

--- Kattis_Lost_Map/lostMap.py
+++ Kattis_Lost_Map/lostMap.py
@@ -1,46 +1,37 @@
-"""
-The code is implemented into the Sample input 1 from Kattis.
-4
-0 1 1 2
-1 0 2 3
-1 2 0 3
-2 3 3 0
-"""
+n = int(input())
 
-n = 4
+table = []
+
+for _ in range(n):
+    line = list(map(int, input().split()))
+    table.append(line)
 
 
-table = [
-    [0 ,1 ,1 ,2]
-    ,[1, 0 ,2 ,3]
-    , [1,2,0,3]
-    , [2,3,3 ,0]
-]
+visited = [False]*n
+min_dist = [float('inf')]*n
+parent = [-1]*n
 
-graph = []
+min_dist[0] = 0
+edges = []
 
-for i in range(n):
-    for j in range(n):
-        dist = table[i][j]
-        graph.append([i,j, dist])
+for _ in range(n):
+    # je cherche un nouveau parent
+    u = -1
+    for v in range(n):
+        if not visited[v] and (u == -1 or min_dist[v] < min_dist[u]):
+            u = v
 
-adj_list = []
+    visited[u] = True
+    if parent[u] != -1:
+        edges.append((parent[u], u))
 
-
-
-
-
-
-
-
-
-
+    for v in range(n):
+        # Avec ce nouveau parent u tu as trouvé une distance plus petite.
+        if not visited[v] and table[u][v] < min_dist[v]:
+            min_dist[v] = table[u][v]
+            parent[v] = u
 
 
+for i in range(n-1):
 
-
-
-
-
-
-
+    print(edges[i][0] + 1, edges[i][1] + 1)
