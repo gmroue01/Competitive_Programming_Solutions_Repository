d01819c911a330c5fb7aeb2021328446d5e71be0
gmroue01 <95682994+gmroue01@users.noreply.github.com>
2026-08-07 11:17:38 +0200

    solution (rice judge) : the algoritm solution of the kattis rice judge problem. We only need to sort the list of rice either by the sum of the size and the amount and the size either by the total exact sum and the rice size

---
+++ kattis_rice_judge/rice_judge.py
@@ -0,0 +1,16 @@
+N = int(input())
+pref = input()
+rices = []
+for i in range(N):
+    A, S = map(int, input().split())
+    rices.append([A, S, A*S, i])
+
+
+if pref == "antal":
+    rices_sorted_by_score = sorted(
+        rices, key=lambda x: (x[2], x[0]), reverse=True)
+else:
+    rices_sorted_by_score = sorted(
+        rices, key=lambda x: (x[2], x[1]), reverse=True)
+
+print(rices_sorted_by_score[0][-1])
